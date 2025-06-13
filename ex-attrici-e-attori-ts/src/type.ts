bd328fe6caff68df9fa511af7ff25f658e688a2c
manuel lantieri <manuel.lantieri02@gmail.com>
2025-06-13 10:04:34 +0200

    milestone 1

---
+++ ex-attrici-e-attori-ts/src/type.ts
@@ -0,0 +1,10 @@
+type person = {
+  readonly id: number;
+  readonly name: string;
+  birth_year: number;
+  death_year?: number;
+  biography: string;
+  image: string;
+};
+
+export type { person };
